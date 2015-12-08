486fa316d533c06217d37dc7103bc22241d1e59b
randy-marsh <juanignacio.fraile@edu.uah.es>
2015-12-08 12:58:37 +0100

    first commit

---
+++ cpu/dev/sound.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015, juan -
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef SOUND_H_
+#define SOUND_H_
+
+
+/* Sound 1 Mode Registers */
+#define NR10	0xFF10
+#define NR11	0xFF11
+#define	NR12	0xFF12
+#define NR13	0xFF13
+#define NR14	0xFF14
+
+/* Sound 2 Mode Registers */
+#define NR21	0xFF16
+#define NR22	0xFF17
+#define NR23	0xFF18
+#define NR24	0xFF19
+
+/* Sound 3 Mode Registers */
+#define NR30	0xFF1A
+#define NR31	0xFF1B
+#define NR32	0xFF1C
+#define NR33	0xFF1D
+#define NR34	0xFF1E
+
+/* Sound 4 Mode Registers */
+#define NR41	0xFF20
+#define NR42	0xFF21
+#define	NR43	0xFF22
+#define NR44	0xFF23
+
+/* Sound Control Registers */
+#define NR50	0xFF24
+#define NR51	0xFF25
+#define NR52	0xFF26
+
+
+#endif /* SOUND_H_ */
